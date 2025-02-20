b4b3bd415e10a7508cc9127e825b2dd9111cc0da
TeddyMuli <tmuli974@gmail.com>
2025-02-20 20:24:34 +0300

    seprate data into x and y

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # first_ml_project
+## Load Data
+### Data Separation as X and Y
+
--- main.py
+++ main.py
@@ -1,5 +1,13 @@
 #!/usr/bin/env python3
 
 import pandas as pd
+'''Data Preparation'''
+# Load Data
 df = pd.read_csv('https://raw.githubusercontent.com/dataprofessor/data/refs/heads/master/delaney_solubility_with_descriptors.csv')
-print(df['logS'])
+
+# Separate x and y
+y = df['logS']
+x = df.drop('logS', axis=1)
+print(x)
+
+# Data splitting
